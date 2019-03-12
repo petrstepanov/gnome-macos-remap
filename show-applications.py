c34baa631bd88bf3698799756293096bf7e1d06c
Petr Stepanov <stepanovps@gmail.com>
2019-03-12 11:36:24 -0400

    First commit

---
+++ show-applications.py
@@ -0,0 +1,17 @@
+# Enter script code
+
+#keyboard.press_key('<super>')
+#keyboard.fake_keypress('a')
+#keyboard.release_key('<super>')
+
+import os
+time.sleep(0.1)
+os.system('show-applications.sh')
+#os.system('/home/petrstepanov/bin/xdotool.sh')
+#system.exec_command("/home/petrstepanov/bin/xdotool.sh", getOutput=False)
+
+#time.sleep(.1)
+
+#keyboard.fake_keypress('<super>+a')
+#output = system.exec_command("show-applications.sh",getOutput=True)
+#system.create_file('/home/petrstepanov/myListing.txt', contents=output)
